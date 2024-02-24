f6f9452fd392746a2be4c8ae0e25934db0ca1e55
301Bri <90125708+301Bri@users.noreply.github.com>
2024-02-24 16:11:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
 const app = express();
 
