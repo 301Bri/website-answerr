b93df0514207500498e4cf28dd46df583478bc15
301Bri <90125708+301Bri@users.noreply.github.com>
2024-02-28 15:55:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static('public'));
 const users = [
     { username: 'user', password: 'Welcome1' },
     { username: 'ari', password: 'ariisstupid' },
-    { username: 'justin', password: 'justinisstupid' },
+    { username: 'justin', password: 'justinisprettierthanari' },
     { username: 'ari2', password: 'ari對葉心動' },
     
     // Add more users as needed
